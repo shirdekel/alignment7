aed597bef89fc64868057f474e54af7258de23bc
Shir Dekel <shirdekel@yahoo.com.au>
2021-05-17 19:04:03 +1000

    Add data

---
+++ R/alignment7.R
@@ -0,0 +1,48 @@
+#' alignment7
+#' A data package for alignment7.
+#' @docType package
+#' @aliases alignment7-package
+#' @title Package Title
+#' @name alignment7
+#' @description A description of the data package
+#' @details Use \code{data(package='alignment7')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{data}
+NULL
+
+
+
+
+#' Detailed description of the data
+#' @name data
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{tbl_df} containing the following fields:
+#' \describe{
+#' \item{id}{}
+#' \item{subject}{}
+#' \item{sex}{}
+#' \item{age}{}
+#' \item{other}{}
+#' \item{business_exp}{}
+#' \item{business_edu}{}
+#' \item{project}{}
+#' \item{alignment}{}
+#' \item{reliability_type}{}
+#' \item{reliability_amount}{}
+#' \item{npv_cond}{}
+#' \item{npv_amount}{}
+#' \item{total_time}{}
+#' \item{order}{}
+#' \item{ranking}{}
+#' \item{allocation}{}
+#' \item{reliability_type_order}{}
+#' \item{alignment_order}{}
+#' \item{sample}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{alignment7}
+NULL
